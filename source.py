c5926415d2e0a9a6a232ca006441353ae0fc948f
TPeterW <peterwangtao0@hotmail.com>
2017-11-06 18:34:13 -0500

    Works

--- source.py
+++ source.py
@@ -5,11 +5,12 @@
 Only going to work for Spring 2018
 '''
 
+import time
 import requests
-import mechanicalsoup
 from getpass import getpass
+from datetime import datetime, timedelta
 
-term = 201823
+term = 201820
 
 def main():
 	sid = input('Please input your 8 digit student ID: ')
@@ -24,9 +25,15 @@ def main():
 		else:
 			crns.append(crn)
 
-	cookies = firstStep(sid, pin)
+	year = int(input('Input year of registration: (e.g. 2017): '))
+	month = int(input('Input month of registration: (e.g. 1 for Jan, 2 for Feb): '))
+	day = int(input('Input day of registration: '))
 
-	lastStep(crns, cookies)
+	waitTillTime(year, month, day)
+
+	while True:
+		cookies = firstStep(sid, pin)
+		lastStep(crns, cookies)
 
 	return
 
@@ -89,45 +96,81 @@ def lastStep(crns, cookies):
 	# with open('register.html', 'w') as outputfile:
 	# 	outputfile.write(resp.text)
 
+def waitTillTime(year, month, day):
+	'''
+	When called, delays the script until 7AM on the
+	given date (day of registration) where date is a
+	string of the form "MM/DD/YYYY".
+	'''
+	# get current time and user supplied date
+	now = datetime.now()
+	registration = datetime(year=year, month=month, day=day, hour=7)
+	
+	time_diff = registration - now
+	
+	while registration > now:
+		now = datetime.now()
+		time_diff = registration - now
+		if registration > now:
+			remaining = datetime(1,1,1) + time_diff if time_diff else datetime(0,0,0)
+			print("%02d:%02d:%02d:%02d until registration (%s @ 7AM)..." % (
+						remaining.day - 1, 
+						remaining.hour, 
+						remaining.minute, 
+						remaining.second, 
+						registration.date()))
+			# remove the following 3 lines for Windows
+			CURSOR_UP_ONE = '\x1b[1A'
+			ERASE_LINE = '\x1b[2K'
+			print(CURSOR_UP_ONE + CURSOR_UP_ONE + ERASE_LINE)
+			try:
+				if time_diff < timedelta(seconds=10):
+					time.sleep(0.001)
+				else:
+					time.sleep(1)
+			except KeyboardInterrupt:
+				print('\nBye!\n')
+				exit(1)
+
 if __name__ == '__main__':
 	main()
 
-def unused():
-	br = mechanicalsoup.StatefulBrowser(user_agent=u_agent)
-	page = br.get('https://ssb-prod.ec.middlebury.edu/PNTR/twbkwbis.P_WWWLogin?')
+# def unused():
+# 	br = mechanicalsoup.StatefulBrowser(user_agent=u_agent)
+# 	page = br.get('https://ssb-prod.ec.middlebury.edu/PNTR/twbkwbis.P_WWWLogin?')
 
-	data = {'sid': sid, 'PIN': pin}
-	resp = br.post('https://ssb-prod.ec.middlebury.edu/PNTR/twbkwbis.P_ValLogin', data)
+# 	data = {'sid': sid, 'PIN': pin}
+# 	resp = br.post('https://ssb-prod.ec.middlebury.edu/PNTR/twbkwbis.P_ValLogin', data)
 
-	# menu before selecting terms
-	resp = br.open('https://ssb-prod.ec.middlebury.edu/PNTR/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu')
+# 	# menu before selecting terms
+# 	resp = br.open('https://ssb-prod.ec.middlebury.edu/PNTR/twbkwbis.P_GenMenu?name=bmenu.P_RegMnu')
 
-	for link in br.links():
-		if link.text == 'Register or Add/Drop Classes':
-			resp = br.follow_link(link)
+# 	for link in br.links():
+# 		if link.text == 'Register or Add/Drop Classes':
+# 			resp = br.follow_link(link)
 
-	# choose term
-	# data = {'term_in': '201820'}
-	# data = {'term_in': '201823'}
-	# resp = br.post('https://ssb-prod.ec.middlebury.edu/PNTR/bwskfreg.P_AltPin', data)
+# 	# choose term
+# 	# data = {'term_in': '201820'}
+# 	# data = {'term_in': '201823'}
+# 	# resp = br.post('https://ssb-prod.ec.middlebury.edu/PNTR/bwskfreg.P_AltPin', data)
 
-	form = br.select_form(nr=1)
-	# form.set_select({'term_in': '201820'})
-	form.set_select({'term_in': '201823'})
-	resp = br.submit_selected()
+# 	form = br.select_form(nr=1)
+# 	# form.set_select({'term_in': '201820'})
+# 	form.set_select({'term_in': '201823'})
+# 	resp = br.submit_selected()
 
 
-	# register now
-	# crns = [21725, 21709, 22494]
-	crns = [22494]
+# 	# register now
+# 	# crns = [21725, 21709, 22494]
+# 	crns = [22494]
 
-	cookie_str = ''
-	for cookie in br.get_cookiejar():
-		cookie_str += cookie.name + '=' + cookie.value + '; '
+# 	cookie_str = ''
+# 	for cookie in br.get_cookiejar():
+# 		cookie_str += cookie.name + '=' + cookie.value + '; '
 
-	lastStep(crns, cookie_str)
+# 	lastStep(crns, cookie_str)
 
-	with open('index.html', 'w') as outputfile:
-		outputfile.write(resp.content.decode("utf-8"))
+# 	with open('index.html', 'w') as outputfile:
+# 		outputfile.write(resp.content.decode("utf-8"))
 
-	br.close()
+# 	br.close()
